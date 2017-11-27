1e1c1e5476824eab1d69bca0d4a31bd80dd3267f
airetriton <wobbe_espinoza@hotmail.com>
2017-11-26 23:10:53 -0700

    one

---
+++ liri.js
@@ -0,0 +1,9 @@
+var keys = require("keys.js");
+var nodeArg = process.argv;
+var "my-tweets" = process.argv[2];
+var "spotify-this-song" = process.argv[3];
+var "movie-this" = process.argv[4];
+var "do-what-it-says" = process.argv[5];
+
+
+
