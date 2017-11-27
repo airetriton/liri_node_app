a6159318fda6f01760bdee10a33b88133d8c431b
airetriton <wobbe_espinoza@hotmail.com>
2017-11-27 00:25:47 -0700

    four

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
-var Twitter = require("twitter");
+// var Twitter = require("twitter");
 
-JavaScript
+// JavaScript
 console.log('this is loaded');
 
 var twitterKeys = {
   consumer_key: 'cGX9GRGVRaXovct3Y6NNLC3zl',
   consumer_secret: 'J2NLFQYohMYRm3ztI8OzkR8FowSUGNa2yuNUT5Z4uHnioYnkyb',
   access_token_key: '934991897206054912-K96ZssIr7xz4cpBoHpMBOe2a8mYMVu6',
-  access_token_secret: 'Q9RofwOmWYz7Ec4ADJiCgitbkdPx2RCreV7sFzcx0B9k',
+  access_token_secret: 'Q9RofwOmWYz7Ec4ADJiCgitbkdPx2RCreV7sFzcx0B9k'
 }
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -1,9 +1,81 @@
-var keys = require("keys.js");
+var keys = require("./keys.js");
+var twitter = require("twitter");
+var omdb = require("request");
+var spotify = require("spotify");
+var fs = require("fs");
+
 var nodeArg = process.argv;
-var "my-tweets" = process.argv[2];
-var "spotify-this-song" = process.argv[3];
-var "movie-this" = process.argv[4];
-var "do-what-it-says" = process.argv[5];
+var liriCommand = process.argv[2];
+var searchTitle = process.argv[3];
+
+
+var twitterKeys = keys.twitterKeys;
+
+var twitterUser = new twitter({
+    consumer_key: twitterKeys.consumer_key,
+    consumer_secret: twitterKeys.consumer_secret,
+    access_token_key: twitterKeys.access_token_key,
+    access_token_secret: twitterKeys.access_token_secret
+});
+
+console.log(twitterUser);
+
+// switch(liriCommand) {
+//     case "my-tweets":
+//     myTweets();
+//     break;
+
+//     case "spotify-this-song":
+//     myPlayList();
+//     break;
+
+//     case "movie-this":
+//     myMovie();
+//     break;
+
+//     case "do-what-it-says":
+//     randomPick();
+//     break;
+// }
+
+
+// function myTweets(){
+//     twitterUser.get('search/tweets', {q: 'aireEspinoza', count: 20}, function(error, tweet, response) {
+//         if(error){
+//             console.log(error);
+//         }else{
+//             console.log(tweet);
+//         }
+//     });
+// };
+
+
+
+
+
+
+//spotify
+
+// var userSong;
+//  if (process.argv[3] === undefined || process.argv[3]=== " "){
+//             userSong = "The Sign";
+//         } else{
+//                 userSong = process.argv[3] 
+//             }
+
+// spotify.search({ type: 'track', query: userSong }, function(err, data) {
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
