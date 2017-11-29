2a55793fc43e54ea435f6d7bb099287e843b23d4
airetriton <wobbe_espinoza@hotmail.com>
2017-11-28 23:50:23 -0700

    eigth

--- keys.js
+++ keys.js
@@ -9,11 +9,3 @@ var twitterKeys = {
 };
 
 module.exports = twitterKeys;
-
-
-// var spotifyKeys = {
-// 	client_ID: 'e6ec134009e54a57b886e8b829fec65c',
-// 	client_secret: '9b9cb5ad5c5b46588cb216ef70067da8'
-// };
-
-// module.exports = spotifyKeys;
--- liri.js
+++ liri.js
@@ -1,9 +1,15 @@
 var twitter = require("twitter");
 var keys = require("./keys.js");
+var t = new twitter(keys);
+
 var appexp = require("express"); 
 
 var omdb = require("request");
-var spotify = require("spotify");
+
+var spotify = require("node-spotify-api");
+var keys2 = require("./keysspot.js")
+var s = new spotify(keys2);
+
 var fs = require("fs");
 
 var nodeArg = process.argv;
@@ -11,34 +17,98 @@ var liriCommand = process.argv[2];
 var searchTitle = process.argv[3];
 
 
-var twitterKeys = keys.twitterKeys;
-
-var t = new twitter(keys);
-
-console.log(t);
 
 
-//accessing twitter
 
-function myTweets(){
-    t.get('search/tweets', {q: 'aireEspinoza', count: 20}, function(error, tweet, response) 
-    {
-        if(error){
-            console.log(error);
-        }else{
-            for (var i = 0; i < tweet.length; i++) {
-            console.log("================================");
-            console.log(tweet[i].text);
-            console.log(tweet[i].created_at);
+
+// console.log(t);
+// console.log(s);
+
+
+//accessing twitter - it works
+//last 20 tweets & when they were created
+
+// function myTweets(){
+	
+
+//     t.get('search/tweets', {q: 'aireEspinoza', count: 20}, function(error, tweet, response) 
+//     {
+//         if(error){
+//             console.log(error);
+//         }else{
+
+//         	var tweet = tweet.statuses
+
+//         	// console.log(tweet);
+
+//             for (var i = 0; i < tweet.length; i++) {
+//             console.log("================================");
+//             console.log(tweet[i].text);
+//             console.log(tweet[i].created_at);
                 
-                var tweetObject = {text: tweet[i].text, created: tweet[i].created_at};
-                fs.appendFileSync("log.txt", JSON.stringify(tweetObject, null, 2));;
-            }    
-        }
-    });
-};
+//                 var tweetObject = {text: tweet[i].text, created: tweet[i].created_at};
+//                 fs.appendFileSync("log.txt", JSON.stringify(tweetObject, null, 2));;
+//             }    
+//         }
+//     });
+// };
 
-myTweets();
+// myTweets();
+
+
+// spotify
+// * This will show the following information about the song in your terminal/bash window
+     
+//      * Artist(s)
+     
+//      * The song's name
+     
+//      * A preview link of the song from Spotify
+     
+//      * The album that the song is from
+
+//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
+
+function spotifyThisSong() {
+	
+	var userSong;
+ 	
+ 	if (process.argv[3] === undefined || process.argv[3] === " "){
+		userSong = "The Sign";
+
+    } else{
+    	userSong = process.argv[3] 
+    }
+
+	s.search({ q: 'userSong', type: 'track' }, function(err, data) {
+  		if (err) {
+    	return console.log('Error occurred: ' + err);
+
+  		} else {
+
+  			var data = tracks.items;
+  			var artist = data[i].album.artists.name;
+  			var album = data[i].album.name;
+  			var songName = data[i].name;
+  			var songURL = data[i].preview_url;
+
+  			for (var i = 0; i < data.length; i++) {
+            console.log("================================");
+            console.log(artist);
+            console.log(songName);
+            console.log(songURL);
+            console.log(album);
+                
+                var dataObject = {Artist: artist, Song: songName, Song Preview: songURL, Album: album};
+                fs.appendFileSync("log.txt", JSON.stringify(dataObject, null, 2));;
+            } 
+  		}
+  	})	 
+};
+ spotifyThisSong();	
+
+
+
 
 // switch(liriCommand) {
 //     case "my-tweets":
@@ -62,16 +132,7 @@ myTweets();
 
 
 
-//spotify
 
-// var userSong;
-//  if (process.argv[3] === undefined || process.argv[3]=== " "){
-//             userSong = "The Sign";
-//         } else{
-//                 userSong = process.argv[3] 
-//             }
-
-// spotify.search({ type: 'track', query: userSong }, function(err, data) {
 
 
 
