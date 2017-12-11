f757fe0eb98925ec9cb30463977bbbeab5680a37
airetriton <wobbe_espinoza@hotmail.com>
2017-12-10 22:18:00 -0700

    seventeen

--- liri.js
+++ liri.js
@@ -41,13 +41,6 @@ function myTweets(){
 };
 
 // myTweets();
-// spotify **	works***
-// * This will show the information about the song in your terminal/bash window
-//      * Artist(s)
-//      * The song's name
-//      * A preview link of the song from Spotify
-//      * The album that the song is from
-//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
 
 function spotifyThisSong() {
 	
@@ -66,7 +59,7 @@ function spotifyThisSong() {
     searchTitle = process.argv[3];
 
   };
-    console.log(searchTitle);
+    // console.log(searchTitle);
    	
 //search spotify for a specific track or the default
 	s.search({
@@ -80,31 +73,31 @@ function spotifyThisSong() {
       console.log('Error occurred: ' + err);
       return;
 
-      } else {
+    } else {
 
-        console.log("hello")
-        console.log(data.tracks.items)
+      // console.log("hello")
+      // console.log(data.tracks.items)
 
-       	//creat a loop to go through all of the songs
+     	//creat a loop to go through all of the songs
 
-  			for (var i = 0; i < data.tracks.items.length; i++) {
-          console.log("hello2", JSON.stringify(data.tracks.items[0]))
+			for (var i = 0; i < data.tracks.items.length; i++) {
+        // console.log("hello2", JSON.stringify(data.tracks.items[0]))
 
-          var songInfo = data.tracks.items[i];
-          console.log("hello3", songInfo.artists)
-          var artist = songInfo.artists[0].name;
-          var album = songInfo.album.name;
-          var songName = songInfo.name;
-          var songURL = songInfo.preview_url;
-            console.log("================================");
-            console.log(artist);
-            console.log(songName);
-            console.log(songURL);
-            console.log(album);
-         //create a variable that holds all of the data so it can go into a file       
-        var dataObject = {Artist: artist, Song: songName, Preview: songURL, Album: album};
-        //put data into a file        
-        fs.appendFileSync("log.txt", JSON.stringify(dataObject, null, 2));;
+        var songInfo = data.tracks.items[i];
+        // console.log("hello3", songInfo.artists)
+        var artist = songInfo.artists[0].name;
+        var album = songInfo.album.name;
+        var songName = songInfo.name;
+        var songURL = songInfo.preview_url;
+          console.log("================================");
+          console.log(artist);
+          console.log(songName);
+          console.log(songURL);
+          console.log(album);
+       //create a variable that holds all of the data so it can go into a file       
+      var dataObject = {Artist: artist, Song: songName, Preview: songURL, Album: album};
+      //put data into a file        
+      fs.appendFileSync("log.txt", JSON.stringify(dataObject, null, 2));;
       
       }
     	}
@@ -131,86 +124,96 @@ var movieName;
 
   };
 
-console.log(movieName);
+// console.log(movieName);
 // Then run a request to the OMDB API with the movie name specified
 var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=8ed601e1";
 
 
 // This line is just to help us debug against the actual URL.
-console.log(queryUrl);
+// console.log(queryUrl);
 
 // Then create a request to the queryUrl
 
 request(queryUrl, function(error, response, body){
-  console.log(response, body, error)
+  // console.log(response)
 
   if(error){
     console.log('Error occurred: ' + error);
      return;
   }
 
-  // if(error && response.statusCode === 200) {
-  //   console.log('Error occurred: ' + error);
-  //    return;
-  // } else {
-    console.log("hello")
-    console.log(movieName)
+    // console.log("hello")
+    // console.log(movieName)
+    // console.log("hello2", JSON.stringify(body))
+    // console.log("hello3", JSON.parse(body))
 
-          console.log("hello2", JSON.stringify(response))
-           
-          // var songInfo = response;
-          console.log("hello3", response.body)
-          var title = response.title;
-          var year = response.year;
-          var imdbRating = response.ratings.value[0];
-          var rottenTomsRating = response.ratings.value[1];
-          var countryProduction = response.country;
-          var language = response.language;
-          var plot = response.plot;
-          var actors = response.actors;
-            console.log("================================");
-            console.log(title);
-            console.log(year);
-            console.log(imdbRating);
-            console.log(rottenTomsRating);
-            console.log(countryProduction);
-            console.log(language);
-            console.log(plot);
-            console.log(actors);
-            console.log("================================")
-//          //create a variable that holds all of the data so it can go into a file       
-        var dataMovie = {Title: title, Year: year, IMDB: imdbRating, 'Rotten Tomatoes': rottenTomsRating, 'Country of Production': countryProduction, Language: language, Plot: plot, Actors: actors,};
-//         //put data into a file        
-        fs.appendFileSync("log.txt", JSON.stringify(dataMovie, null, 2));;
+    var songInfo = JSON.parse(body)
+    // console.log("hello4", songInfo.Title)
 
-        
-    // }
+    var title = songInfo.Title;
+    var year = songInfo.Year;
+    var rating = songInfo.Ratings;
+    var imdbRating = rating[0].Source.Value;
+    var rottenTomsRating = rating[1].Source.Value;
+    var countryProduction = songInfo.Country;
+    var language = songInfo.Language;
+    var plot = songInfo.Plot;
+    var actors = songInfo.Actors;
+      console.log("================================");
+      console.log(title);
+      console.log(year);
+      console.log(imdbRating);
+      console.log(rottenTomsRating);
+      console.log(countryProduction);
+      console.log(language);
+      console.log(plot);
+      console.log(actors);
+      console.log("================================")
+        //create a variable that holds all of the data so it can go into a file       
+    var dataMovie = {"Title": title, "Year": year, "IMDB Rating": imdbRating,"Rotten Tommatos Rating": rottenTomsRating, 'Country of Production': countryProduction, "Language": language, "Plot": plot, "Actors": actors};
+        //put data into a file    
+      // console.log(dataMovie)    
+    fs.appendFileSync("log.txt", JSON.stringify(dataMovie, null, 2));;
+  
   })
 };
-movieThis();
+// movieThis();
+
+function randomPick {
+  fs.readFile("random.txt", "UTF-8", function(error, data){
+    if (error){
+      console.log(error);
+    }else{
+      console.log(data);
+    }
+  })
+
+}
 
 
 
+// create the liri commands
+switch(liriCommand) {
+    case "my-tweets":
+    myTweets();
+    break;
 
+    case "spotify-this-song":
+    spotifyThisSong();
+    break;
 
-//create the liri commands
-// switch(liriCommand) {
-//     case "my-tweets":
-//     myTweets();
-//     break;
+    case "movie-this":
+    movieThis();
+    break;
 
-//     case "spotify-this-song":
-//     spotifyThisSong();
-//     break;
+    case "do-what-it-says":
+    randomPick();
+    break;
 
-//     case "movie-this":
-//     movieThis();
-//     break;
-
-//     case "do-what-it-says":
-//     randomPick();
-//     break;
-// }
+    default:
+    msg = "It won't work if you don't enter a command.  Have a nice day.";
+    break;
+}
 
 
 
